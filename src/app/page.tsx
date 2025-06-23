fcebb5d041dd454643a731448814611ff0358dde
Reza Nazeri <reza.armani75@gmail.com>
2025-06-23 17:31:56 +0330

    add .nojekyll file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>Enolight</h1>;
+  return (
+    <div>
+      <h1>Enolight</h1>
+      <h2 className="text-red-500">Enolight</h2>
+    </div>
+  );
 }
